6e73a1ad099f9d473b948cc277e3cfc029173ba9
AnishaZ <anishagzacharia@gmail.com>
2017-04-17 21:09:29 -0700

    Added testing for relationships to user_spec for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,6 +11,7 @@ RSpec.describe User, type: :model do
       expect(create(:user, email: 'EMAIL@GMAIL.COM').email).to eq('email@gmail.com')
     end
   end
+
   context "with invalid attributes should not save if" do
     it 'name is blank' do
       expect(build(:user, name: '')).to be_invalid
@@ -33,6 +34,23 @@ RSpec.describe User, type: :model do
     end
     it "password doesn't match password_confirmation" do
       expect(build(:user, password_confirmation: 'notpassword')).to be_invalid
+    end
+  end
+
+  context 'relationships' do
+    before do
+      @user = create(:user)
+      @secret = create(:secret, content: 'secret 1', user: @user)
+      @like = create(:like, secret: @secret, user: @user)
+    end
+    it 'has secrets' do
+      expect(@user.secrets).to include(@secret)
+    end
+    it 'has likes' do
+      expect(@user.likes).to include(@like)
+    end
+    it "has secrets through likes table" do
+      expect(@user.secrets_liked).to include(@secret)
     end
   end
 end
